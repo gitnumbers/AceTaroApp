d854023e0996359df20b499670064e676e61c521
soo <44661519+gitnumbers@users.noreply.github.com>
2024-05-02 18:15:14 +0900

    update page.js

--- App.js
+++ App.js
@@ -1,20 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import CardPage from './pages/CardPage';
+import MainPage from './pages/MainPage';
+import DetailPage from './pages/DetailPage';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default function App(){
+  //return (<MainPage/>)
+  return (<DetailPage/>)
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
